2e5aa6ac20f2120f38bb89b527df8252af329a60
Patricia Alencar <139386534+Patt-Alencar@users.noreply.github.com>
2024-03-18 20:01:21 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,41 @@
-window.addEventListener('load', () => {
-   
-    let $btnAdd = document.querySelector('#btn-add')
+window.addEventListener('load',()=>{
+    
+    let $btnAdd = document.querySelector("#btn-add")
+    let $btnSalvar = document.querySelector("#btn-salvar")
+    let $btnCancelar = document.querySelector("#btn-cancelar")
 
-    $btnAdd.addEventListener('click', activeModal)
+    $btnAdd.addEventListener('click',activeModal)
+    $btnSalvar.addEventListener('click',salvarTarefa)
+    $btnCancelar.addEventListener('click',cancelar)
+
 })
 
 function activeModal(){
-    let $modal = document.querySelector('#modal')
-    $modal.classList.toggle('modal-active')
+    let $modal = document.querySelector("#modal")
+    $modal.classList.toggle("modal-active")
+}
 
+function salvarTarefa(event){
+    event.preventDefault()
+    let $listaTarefa = document.querySelector("#listaTarefa")
+    let $task = document.querySelector("#task")
+    let tarefaAtual = $task.value
+    $listaTarefa.innerHTML+=`
+    <div class="itemtarefa">
+        <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+        </div>
+        <h3>
+            ${tarefaAtual}
+        </h3>
+        <button type="button" class="btn btn-danger">Danger</button>
+    </div>
+    `
+    
+} 
+
+function cancelar(event){
+    event.preventDefault()
+    let $modal = document.querySelector("#modal")
+    $modal.classList.remove("modal-active")
 }
